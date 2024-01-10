9863502dbeb8722219ef393d2202882ddb3d8c04
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-01-10 17:26:42 +0200

    Added MubBlazor lib to WebUI project

--- OnlineShop.WebUI/Program.cs
+++ OnlineShop.WebUI/Program.cs
@@ -1,22 +1,22 @@
+using MudBlazor.Services;
 using OnlineShop.WebUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
