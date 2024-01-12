f5f077981fd0f0aef76dc020cc215da0c977164b
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-01-12 13:47:27 +0200

    Added ProductDto and ProductEntity models

---
+++ OnlineShop.Domain/DTOs/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Domain.DTOs
+{
+    public class ProductDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Unit { get; set; } = string.Empty;
+    }
+}
---
+++ OnlineShop.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Domain.Entities
+{
+    public class ProductEntity
+    {
+        public Guid ProductId { get; set; } = Guid.NewGuid();
+        [Required]
+        public string ProductName { get; set; } = string.Empty;
+        [Required]
+        public string ProductDescription { get; set; } = string.Empty;
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Unit { get; set; } = string.Empty;
+    }
+}
