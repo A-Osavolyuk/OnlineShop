7a8e73ce3f9cd372032cb2118dd3ea4846525c66
A.Osavoliuk <sasha.osavoll11@gmail.com>
2024-01-12 14:16:45 +0200

    Added IProiductService

---
+++ OnlineShop.Application/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using LanguageExt.Common;
+using OnlineShop.Domain.DTOs;
+using OnlineShop.Domain.Entities;
+
+namespace OnlineShop.Application.Interfaces
+{
+    public interface IProductService
+    {
+        public Task<Result<IEnumerable<ProductEntity>>> GetAll();
+        public Task<Result<ProductEntity>> GetById(Guid id);
+        public Task<Result<ProductEntity>> Create(ProductDto product);
+        public Task<Result<ProductEntity>> Update(ProductDto product, Guid id);
+        public Task<Result<bool>> Delete(Guid id);
+    }
+}
